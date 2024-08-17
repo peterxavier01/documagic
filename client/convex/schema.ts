66aba6676446de160c41bb4fc37fff54d6b5acdf
Peter Uadiale <fxavy02@gmail.com>
2024-08-18 00:09:22 +0100

    chore: add new table -> conversions

--- client/convex/schema.ts
+++ client/convex/schema.ts
@@ -2,11 +2,6 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  tasks: defineTable({
-    text: v.string(),
-    isCompleted: v.boolean(),
-  }),
-
   users: defineTable({
     name: v.string(),
     // this the Clerk ID, stored in the subject JWT field
@@ -15,4 +10,12 @@ export default defineSchema({
     image: v.optional(v.string()),
     uploaded_documents: v.optional(v.array(v.string())),
   }).index("byExternalId", ["externalId"]),
+
+  conversions: defineTable({
+    userId: v.id("users"),
+    documentSize: v.number(),
+    documentName: v.string(),
+    documentId: v.string(),
+    toolUsed: v.string(),
+  }),
 });
