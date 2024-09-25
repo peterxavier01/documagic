180cc6a7fec0fc11ed1bff945fa72115ecf5ba1d
Peter Uadiale <fxavy02@gmail.com>
2024-09-25 17:40:05 +0100

    style: change hero section min height

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   const { isAuthenticated } = useConvexAuth();
 
   return (
-    <div className="flex flex-col w-full items-center justify-center min-h-dvh">
+    <div className="flex flex-col w-full items-center justify-center min-h-[95dvh] md:min-h-dvh">
       <div className="text-center space-y-4 text-white font-poppins wrapper">
         <h1 className="text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-normal font-bold text-white">
           Unlock the <span className="text-coral">Magic</span> of Document
