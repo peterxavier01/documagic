3c7f238040608788f2c35fc17d253c0371e93eb6
Peter Uadiale <fxavy02@gmail.com>
2024-09-29 23:26:11 +0100

    chore: create contact us page

---
+++ client/src/routes/(app)/contact.lazy.tsx
@@ -0,0 +1,25 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+import ContactForm from "@/components/contact-form";
+import Heading from "@/components/heading";
+
+export const Route = createLazyFileRoute("/(app)/contact")({
+  component: Contact,
+});
+
+function Contact() {
+  return (
+    <main className="py-12 min-h-[calc(100dvh-60px)] font-poppins">
+      <section className="section-wrapper">
+        <div className="text-center">
+          <Heading title="Get in Touch" className="text-center pb-2 text-bg" />
+          <p className="text-lg text-dark-gray">
+            Have any questions? Feel free to reach out to us
+          </p>
+        </div>
+
+        <ContactForm />
+      </section>
+    </main>
+  );
+}
