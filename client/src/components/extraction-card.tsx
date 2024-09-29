2544f93fde2e11e71533ff6ee5901c8a619d56bb
Peter Uadiale <fxavy02@gmail.com>
2024-09-29 23:22:22 +0100

    chore: create extraction card component

---
+++ client/src/components/extraction-card.tsx
@@ -0,0 +1,54 @@
+import { getImageURL } from "@/lib/utils";
+import { Doc, Id } from "../../convex/_generated/dataModel";
+
+import { Card, CardTitle } from "@/components/ui/card";
+import { Button } from "./ui/button";
+import { ArrowUpRight } from "lucide-react";
+import { Link } from "@tanstack/react-router";
+
+interface ExtractionCardProps {
+  item: Doc<"conversions">;
+}
+
+export default function ExtractionCard({ item }: ExtractionCardProps) {
+  const storageId = item.documentId as Id<"conversions">;
+  const imageURL = getImageURL(storageId);
+
+  return (
+    <Card key={item._id} className="bg-off-white group">
+      <div className="flex gap-4 items-center justify-between">
+        <div className="flex gap-4 items-center">
+          <img
+            src={imageURL}
+            alt={item.documentName}
+            className="size-20 object-cover rounded"
+          />
+
+          <CardTitle className="text-primary font-medium line-clamp-1 md:text-wrap">
+            <Link
+              to="/tools/extraction/$extractionId"
+              params={{ extractionId: item._id }}
+              title={item.documentName}
+            >
+              {item.documentName}
+            </Link>
+          </CardTitle>
+        </div>
+
+        <Link
+          to="/tools/extraction/$extractionId"
+          params={{ extractionId: item._id }}
+          title={item.documentName}
+        >
+          <Button
+            variant="outline"
+            className="hidden transition md:group-hover:flex hover:bg-dark-gray hover:text-white"
+          >
+            <span>View</span>
+            <ArrowUpRight />
+          </Button>
+        </Link>
+      </div>
+    </Card>
+  );
+}
